5e94281fa483c66e0d926d481d8f269c550c1275
stastix <designab33@gmail.com>
2025-01-15 07:06:42 +0100

    aa

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
 };
 module.exports = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
